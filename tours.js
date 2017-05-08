f0b5c304cbae4dcd6c2e7b2ab42c7c4311e2c0b6
alexkodak <neret.jeremy@gmail.com>
2017-05-08 15:12:04 +0100

    Add files via upload

---
+++ tours.js
@@ -0,0 +1,32 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+//var server = new Server('localhost', 27017, {auto_reconnect: true});
+var server = new Server(process.env.MONGODB_URI);
+db = new Db('tourdb', server);
+
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'tourdb' database");
+        db.collection('tours', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'tours' collection doesn't exist. Creating it with sample data...");
+                }
+        });
+    }
+});
+
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving tour: ' + id);
+    db.collection('tours', function(err, collection) {
+        collection.findOne({'_id':new mongo.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
