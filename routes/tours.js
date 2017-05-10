7642fa8de5ffb91d107472638f396c3bd3dfbb60
alexkodak <neret.jeremy@gmail.com>
2017-05-10 13:52:37 +0100

    Add files via upload

--- routes/tours.js
+++ routes/tours.js
@@ -24,7 +24,8 @@ exports.findById = function(req, res) {
     var id = req.params.id;
     console.log('Retrieving tour: ' + id);
     db.collection('tours', function(err, collection) {
-        collection.findOne({'_id':new mongo.ObjectID(id)}, function(err, item) {
+       /* collection.findOne({'_id':new mongo.ObjectID(id)}, function(err, item) { */
+	   collection.findOne({ }, { tour: 1, language: 1 }, function(err, item) {
             res.send(item);
         });
     });
