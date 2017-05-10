3dbbb3aad1e24dda66d8f0902a4aa92c194ce176
alexkodak <neret.jeremy@gmail.com>
2017-05-10 17:57:21 +0100

    Add files via upload

--- routes/tours.js
+++ routes/tours.js
@@ -4,7 +4,9 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server(process.env.MONGODB_URI, 27017, {auto_reconnect: true});
+	
+//var server = new Server(process.env.MONGODB_URI, 27017, {auto_reconnect: true});
+var server = process.env.MONGODB_URI;
 db = new Db('guidebot', server);
 
 
