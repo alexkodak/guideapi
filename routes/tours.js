afc9123f01064abae94e28cb41be0bb594e11677
alexkodak <neret.jeremy@gmail.com>
2017-05-20 11:50:29 +0100

    Add files via upload

--- routes/tours.js
+++ routes/tours.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://alexkodak:pcJ-z39nqLBg@ds111461.mlab.com:11461/guidebot';
+var mongoDB = process.env.MONGODB_URI;
 
 mongoose.connect(mongoDB);
 
